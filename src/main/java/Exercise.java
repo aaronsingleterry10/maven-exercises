933f1805341253b75bf2fc4cf7b93dc07f0c30fe
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-29 16:37:34 -0500

    completed a step

--- src/main/java/Exercise.java
+++ src/main/java/Exercise.java
@@ -1,5 +1,13 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
 public class Exercise {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter something:");
+        String userInput = sc.nextLine();
+        System.out.println(userInput);
+        System.out.println(StringUtils.isNumeric(userInput));
     }
 }
